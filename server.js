6ba9ce0d97880a4a8874225b5dc3d528e201fb6c
Victor Orozco <me@vorozco.com>
2025-03-12 21:57:24 -0300

    Base fastify app

---
+++ server.js
@@ -0,0 +1,36 @@
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+const fastify = Fastify({
+    logger: true
+})
+
+// Declare a route
+fastify.get('/', async function handler (request, reply) {
+
+    return { hello: 'world' }
+})
+
+// Route that triggers a recursive fibonacci function receiving n as param
+fastify.get('/fibonacci/:n', async function handler (request, reply) {
+    const n = request.params.n
+
+    function fibonacci(n) {
+        // Base case
+        if (n <= 1) {
+            return n
+        }
+        // Recursive case
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    }
+
+    const result = fibonacci(n)
+    return { result }
+})
+
+// Run the server!
+try {
+    await fastify.listen({ port: 3000 })
+} catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+}
