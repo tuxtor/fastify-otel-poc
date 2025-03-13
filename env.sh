2d75ae25dc222e715ef058923a09340389575b21
Victor Orozco <me@vorozco.com>
2025-03-12 22:30:21 -0300

    Adding autoinstrumentation option

---
+++ env.sh
@@ -0,0 +1,5 @@
+export OTEL_TRACES_EXPORTER="otlp"
+export OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4318"
+export OTEL_NODE_RESOURCE_DETECTORS="env,host,os"
+export OTEL_SERVICE_NAME="fastify-otel-poc"
+export NODE_OPTIONS="--require @opentelemetry/auto-instrumentations-node/register"
